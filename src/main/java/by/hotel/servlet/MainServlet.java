cd2f1fed419f1b874cb412d8016bfea1aea08b01
drafteee <sergej_-96@mail.ru>
2017-02-23 17:52:50 +0300

    j

---
+++ src/main/java/by/hotel/servlet/MainServlet.java
@@ -0,0 +1,25 @@
+package by.hotel.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by SK on 14.02.2017.
+ */
+@WebServlet (urlPatterns = {"/servlet"})
+public class MainServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException, ServletException {
+        req.setAttribute("attributeName","igor_krasavchik");
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req,resp);
+        //String test = req.getAttribute("name").toString();
+//        req.getRequestDispatcher("/webapp/index.jsp").forward(req,resp);
+
+    }
+}
