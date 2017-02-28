0a4ae0c2d1296473bc491787066861848cf6a247
www.bk.ru@mail.ru <igorekugolek1i>
2017-02-28 17:29:56 +0300

    Correct varibles

--- src/main/java/by/hotel/bd/BD.java
+++ src/main/java/by/hotel/bd/BD.java
@@ -1,7 +1,8 @@
 package by.hotel.bd;
 
+import resource.Constants;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -12,15 +13,14 @@ public class BD {
 
     private static Connection connection;
 
-    private final static String URL = "jdbc:mysql://localhost:3306/Hotel?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true\" +\n" +
-            "                    \"&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "root";
+    private  static String url = Constants.URL;
+    private  static String username = Constants.USERNAME;
+    private  static String password = Constants.PASSWORD;
 
     static{
         try{
 
-            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            connection = DriverManager.getConnection(url,username,password);
         } catch (SQLException e) {
             System.err.println("Problem with connection");
         }
