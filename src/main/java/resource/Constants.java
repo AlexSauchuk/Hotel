5fe0cef59c42f8da48be1bc8c8818fc716ac4b28
www.bk.ru@mail.ru <igorekugolek1i>
2017-02-28 17:33:17 +0300

    Create resource

---
+++ src/main/java/resource/Constants.java
@@ -0,0 +1,11 @@
+package resource;
+
+/**
+ * Created by 1 on 28.02.2017.
+ */
+public class Constants {
+    public final static String USERNAME = "root";
+    public final static String PASSWORD = "root";
+    public final static String URL = "jdbc:mysql://localhost:3306/Hotel?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true\" +\n" +
+            "                    \"&useLegacyDatetimeCode=false&serverTimezone=UTC";
+}
