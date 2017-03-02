8d859514695c73748f64bd79441dcc7debae3ea1
www.bk.ru@mail.ru <igorekugolek1i>
2017-03-02 11:56:30 +0300

    Create constant_test.

---
+++ src/main/java/resources/f.java
@@ -0,0 +1,11 @@
+package resources;
+
+/**
+ * Created by 1 on 02.03.2017.
+ */
+public class f {
+    public final static String USERNAME = "root" ;
+    public final static String PASSWORD = "root";
+    public final static String URL = "jdbc:mysql://localhost:3306/Hotel?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true\" +\n" +
+            "                    \"&useLegacyDatetimeCode=false&serverTimezone=UTC";
+}
