7ab243cc0cddcf822e4e9a5884c81d897a7eac31
Igor23k <www.bk.ru@mail.ru>
2017-04-11 08:11:36 +0300

    Added correct filling with field "Room"

--- src/main/java/by/hotel/dao/daoimpl/ReservationDaoImpl.java
+++ src/main/java/by/hotel/dao/daoimpl/ReservationDaoImpl.java
@@ -1,10 +1,7 @@
 package by.hotel.dao.daoimpl;
 
 
-import by.hotel.bean.Reservation;
-import by.hotel.bean.Room;
-import by.hotel.bean.RoomType;
-import by.hotel.bean.User;
+import by.hotel.bean.*;
 import by.hotel.dao.AbstractDao;
 import by.hotel.dao.ReservationDao;
 import by.hotel.dao.constants.Constants;
@@ -34,20 +31,7 @@ public class ReservationDaoImpl extends AbstractDao implements ReservationDao {
             statement = connection.prepareStatement(Constants.GET_ALL_RESERVATIONS);
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                Reservation reservation = new Reservation();
-                User user = new User();
-                user.setName(resultSet.getString("name"));
-                user.setSurname(resultSet.getString("surname"));
-                user.setMobilePhone(resultSet.getString("mobilePhone"));
-                user.setPassportNumber(resultSet.getString("passportNumber"));
-                user.setSex(resultSet.getString("sex"));
-                reservation.setUser(user);
-                reservation.setRoomNumber(resultSet.getInt("room_number"));
-                reservation.setDateIn(resultSet.getDate("date-in"));
-                reservation.setDateOut(resultSet.getDate("date-out"));
-                reservation.setDaysCount(resultSet.getInt("days_count"));
-
-                reservations.add(reservation);
+                reservations.add(fillReservation(resultSet));
             }
         } catch (SQLException e) {
             throw new DAOException(e);
@@ -115,11 +99,54 @@ public class ReservationDaoImpl extends AbstractDao implements ReservationDao {
 
     private PreparedStatement fillStatement(PreparedStatement statement, Reservation reservation) throws SQLException {
         statement.setInt(1, reservation.getUser().getId());
-        statement.setInt(2, reservation.getRoomNumber());
+        statement.setString(1, reservation.getUser().getPassword());
+        statement.setString(1, reservation.getUser().getMobilePhone());
+        statement.setString(1, reservation.getUser().getName());
+        statement.setString(1, reservation.getUser().getPassportNumber());
+        statement.setString(1, reservation.getUser().getLogin());
+        statement.setString(1, reservation.getUser().getSex());
+        statement.setString(1, reservation.getUser().getSurname());
+        statement.setInt(2, reservation.getRoom().getId());
+        statement.setString(2, reservation.getRoom().getPhone());
+        statement.setInt(2, reservation.getRoom().getFloor());
+        statement.setInt(2, reservation.getRoom().getRoomType().getId());
+        statement.setString(2, reservation.getRoom().getRoomType().getAdditionalInfo());
+        statement.setFloat(2, reservation.getRoom().getRoomType().getCostPerDay());
+        statement.setInt(2, reservation.getRoom().getRoomType().getBedsCount());
+        statement.setInt(2, reservation.getRoom().getRoomType().getRoomsCount());
         statement.setDate(3, reservation.getDateIn());
         statement.setDate(4, reservation.getDateOut());
         statement.setInt(5, reservation.getDaysCount());
 
         return statement;
+    }
+
+    private Reservation fillReservation(ResultSet resultSet) throws SQLException {
+        Reservation reservation = new Reservation();
+        User user = new User();
+        user.setName(resultSet.getString("name"));
+        user.setSurname(resultSet.getString("surname"));
+        user.setMobilePhone(resultSet.getString("mobilePhone"));
+        user.setPassportNumber(resultSet.getString("passportNumber"));
+        user.setSex(resultSet.getString("sex"));
+        reservation.setUser(user);
+        reservation.setDateIn(resultSet.getDate("date-in"));
+        reservation.setDateOut(resultSet.getDate("date-out"));
+        reservation.setDaysCount(resultSet.getInt("days_count"));
+
+        Room room = new Room();
+        RoomType roomType = new RoomType(resultSet.getInt("room_type.id"),
+                resultSet.getInt("rooms_count"),
+                resultSet.getInt("beds_count"),
+                resultSet.getInt("cost_per_day"),
+                resultSet.getString("additional_info"));
+        room.setId(resultSet.getInt("id"));
+        room.setRoomType(roomType);
+        room.setFloor(resultSet.getInt("floor"));
+        room.setPhone(resultSet.getString("phone"));
+
+        reservation.setRoom(room);
+
+        return reservation;
     }
 }
